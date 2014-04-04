c9de07bcfb35d8243816ef6920bbed42dbbc9192
Sergey Aksyonov <sergey@aksyonov.pp.ua>
2014-04-05 00:51:43 +0300

    working authentication and chat

--- public/app.js
+++ public/app.js
@@ -1,70 +1,79 @@
 angular.module('chat', [
-  'ngRoute',
-  'btford.socket-io'
-])
+        'ngRoute',
+        'ngAnimate',
+        'btford.socket-io'
+    ])
 
-.config(function ($routeProvider, $locationProvider) {
-  $locationProvider.html5Mode(true).hashPrefix('!');
-  $routeProvider
-    .when('/', {
-      controller:'MainCtrl',
-      templateUrl:'/templates/main.html'
+    .config(function ($routeProvider, $locationProvider) {
+        $locationProvider.html5Mode(true).hashPrefix('!');
+        $routeProvider
+            .when('/chat', {
+                controller: 'ChatCtrl',
+                templateUrl: '/templates/chat.html'
+            })
+            .when('/login', {
+                controller: 'LoginCtrl',
+                templateUrl: '/templates/login.html'
+            })
+            .when('/logout', {
+                controller: 'LogoutCtrl',
+                template: ' '
+            });
     })
-    .when('/chat', {
-      controller:'ChatCtrl',
-      templateUrl:'/templates/chat.html'
+
+    .factory('socket', function (socketFactory) {
+        var socket = io.connect();
+        var factory = socketFactory({
+            ioSocket: socket
+        });
+        factory.s = socket;
+        return factory;
     })
-    .when('/login', {
-      controller:'LoginCtrl',
-      templateUrl:'/templates/login.html'
+
+    .run(function (socket, $location, $rootScope) {
+        console.log('run');
+        socket.on('login:unauthorized', function () {
+            socket.s.disconnect();
+            $location.path("/login");
+        });
+        socket.on('login:success', function (user) {
+            $location.path("/chat");
+            $rootScope.loggedIn = true;
+        });
     })
-    .when('/logout', {
-      controller:'LogoutCtrl'
+
+    .controller('LoginCtrl', function ($scope, $http, $location, socket) {
+        $scope.login = function () {
+            $http.post('/login', {
+                username: $scope.name,
+                password: $scope.pass
+            }).then(function (res) {
+                if (res.data === 'ok') {
+                    socket.s.socket.connect();
+                } else {
+                    $scope.err = res.data;
+                }
+            });
+        };
     })
-    .otherwise({
-      redirectTo:'/'
+
+    .controller('LogoutCtrl', function ($http, $location, $rootScope, socket) {
+        $http.post('/logout').then(function (res) {
+            socket.s.disconnect();
+            $location.path("/login");
+            $rootScope.loggedIn = false;
+        });
+    })
+
+    .controller('ChatCtrl', function ($scope, socket) {
+        console.log('ctrl');
+        $scope.messages = [];
+        socket.forward('chat', $scope);
+        $scope.$on('socket:chat', function (e, data) {
+            $scope.messages.push(data);
+        });
+        $scope.send = function () {
+            socket.emit('chat', $scope.message);
+            $scope.message = '';
+        };
     });
-})
-
-.factory('socket', function (socketFactory) {
-  var socket = io.connect();
-  var factory = socketFactory({
-    ioSocket: socket
-  });
-  factory.s = socket;
-  return factory;
-})
-
-.controller('MainCtrl', function (socket, $location) {
-  socket.on('login:unauthorized', function () {
-    socket.s.disconnect();
-    $location.path("/login");
-  });
-  socket.on('login:success', function (user) {
-    $location.path("/chat");
-  });
-})
-
-.controller('LoginCtrl', function ($scope, $http, $location, socket) {
-  $scope.login = function () {
-    $http.post('/login', {
-      username: $scope.name,
-      password: $scope.pass
-    }).then(function (res) {
-      if (res.data === 'ok') {
-        socket.s.socket.connect();
-        $location.path("/chat");
-      } else {
-        $scope.err = res.data;
-      }
-    });
-  };
-})
-
-.controller('ChatCtrl', function () {
-
-})
-
-.controller('LogoutCtrl', function () {
-
-});
