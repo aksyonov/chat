26b39eee8d4f5a937e9958bf491f9da85a36ec57
Sergey Aksyonov <sergey@aksyonov.pp.ua>
2014-04-06 01:39:52 +0300

    rooms

--- public/chat/chat.js
+++ public/chat/chat.js
@@ -1,12 +1,34 @@
 angular.module('chatApp.chat', ['luegg.directives'])
     .controller('ChatCtrl', function ($scope, socket) {
+        $scope.rooms = [
+            {
+                name: 'General'
+            }, {
+                name: 'JavaScript'
+            }, {
+                name: 'PHP'
+            }, {
+                name: 'Fun'
+            }
+        ];
         $scope.messages = [];
-        socket.forward('chat', $scope);
-        $scope.$on('socket:chat', function (e, data) {
-            $scope.messages.push(data);
-        });
+        $scope.currentRoom = 0;
+        socket.forward(['chat', 'room:online'], $scope);
+
         $scope.send = function () {
             socket.emit('chat', $scope.message);
             $scope.message = '';
         };
+        $scope.changeRoom = function (id) {
+            $scope.currentRoom = id;
+            socket.emit('room:change', id);
+            $scope.messages = [];
+        };
+
+        $scope.$on('socket:chat', function (e, data) {
+            $scope.messages.push(data);
+        });
+        $scope.$on('socket:room:online', function (e, data) {
+            $scope.rooms[data.room].online = data.value;
+        });
     })
